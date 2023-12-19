889fd44d582c112e6c0641071bcd7685df32b5f5
Stonzim <bryn.carcel@gmail.com>
2023-12-19 19:36:39 +0100

    minor changes

--- myApp/src/test/java/step_definitions/GreenKartSteps.java
+++ myApp/src/test/java/step_definitions/GreenKartSteps.java
@@ -6,14 +6,14 @@ import io.cucumber.java.en.When;
 public class GreenKartSteps {
     @Given("user is on GreenKart landing page")
     public void user_is_on_green_kart_landing_page() {
-        // Write code here that turns the phrase above into concrete actions
+
     }
     @When("user searches with shortname {string} to extract actual name of product")
     public void user_searches_with_shortname_to_extract_actual_name_of_product(String string) {
-        // Write code here that turns the phrase above into concrete actions
+
     }
     @Then("user searches for the same shortname in offers page to check if it exists")
     public void user_searches_for_the_same_shortname_in_offers_page_to_check_if_it_exists() {
-        // Write code here that turns the phrase above into concrete actions
+
     }
 }
