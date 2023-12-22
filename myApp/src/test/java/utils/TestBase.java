d92d8a82d79a9343ae7cc1632bc0261bcd50eb92
Stonzim <bryn.carcel@gmail.com>
2023-12-22 18:30:49 +0100

    messing around

--- myApp/src/test/java/utils/TestBase.java
+++ myApp/src/test/java/utils/TestBase.java
@@ -4,6 +4,9 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.time.Duration;
@@ -20,16 +23,27 @@ public class TestBase {
         properties.load(fileInputStream);
 
         if(driver == null) {
+//            if(properties.getProperty("browser").equalsIgnoreCase("chrome")) {
+//                WebDriverManager.chromedriver().setup();
+//                ChromeOptions options = new ChromeOptions();
+//                options.addArguments("--headless");
+//                options.addArguments("--window-size=1920,1080");
+//                options.addArguments("start-maximized");
+//                options.addArguments("enable-automation");
+//                options.addArguments("--disable-infobars");
+//
+//                driver = new ChromeDriver(options);
+//            }
             if(properties.getProperty("browser").equalsIgnoreCase("chrome")) {
-                WebDriverManager.chromedriver().setup();
-                ChromeOptions options = new ChromeOptions();
+                WebDriverManager.edgedriver().setup();
+                EdgeOptions options = new EdgeOptions();
                 options.addArguments("--headless");
                 options.addArguments("--window-size=1920,1080");
                 options.addArguments("start-maximized");
                 options.addArguments("enable-automation");
                 options.addArguments("--disable-infobars");
 
-                driver = new ChromeDriver(options);
+                driver = new EdgeDriver(options);
             }
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
             driver.get(properties.getProperty("QA-URL"));
